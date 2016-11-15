320fa842b72db7db7c908b1b5aeb398d79597959
聂俊雷 <junlei.nie@hotmail.com>
2016-11-15 15:03:04 +0800

    deploy

---
+++ src/main/resources/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+SERVER_IP=192.168.115.129
+USER=root
+SERVER_PORT=22
+
+
+echo "服务器是"${SERVER_IP}",端口是"$SERVER_PORT
+echo "建立临时拷贝目录"
+ssh -p $SERVER_PORT ${USER}@${SERVER_IP} "rm -rf MyDeploy"
+ssh -p $SERVER_PORT ${USER}@${SERVER_IP} "mkdir -p MyDeploy"
+
+echo "传输打包文件至临时目录"
+#根据自己的目录结构进行改动
+springBoot=/root/.jenkins/workspace/sboot/spring-boot-test-1.0-SNAPSHOT-test.zip
+
+scp -P $SERVER_PORT $springBoot ${USER}@$SERVER_IP:~/MyDeploy
+echo "拷贝业务服务文件成功"
+
+echo "尝试优雅退出服务"
+ssh -p $SERVER_PORT ${USER}@$SERVER_IP "pid=\`ps -ef|grep spring-boot-test|grep -v grep|awk '{print \$2}'\` && sudo kill \$pid"
+
+sleep 3
+echo "3秒后检查服务有无退出，若服务依旧存在，则强行杀掉"
+ssh -p $SERVER_PORT ${USER}@$SERVER_IP "pid=\`ps -ef|grep spring-boot-test|grep -v grep|awk '{print \$2}'\` && [ ! -z $pid ] && sudo kill -9 \$pid"
+
+echo "建立部署目录"
+ssh -p $SERVER_PORT ${USER}@$SERVER_IP "sudo rm -rf /work/MyDeploy"
+echo "文件从临时目录拷贝至部署目录"
+ssh -p $SERVER_PORT ${USER}@$SERVER_IP "sudo cp -r ~/MyDeploy /work/"
+
+echo "解压服务文件"
+ssh -p $SERVER_PORT ${USER}@$SERVER_IP "cd /work/MyDeploy && sudo unzip  spring-boot-test-1.0-SNAPSHOT.jar -d sboot && sudo rm -f spring-boot-test-1.0-SNAPSHOT-test.zip"
+
+echo "启动服务"
+ssh -p $SERVER_PORT ${USER}@$SERVER_IP "cd /work/MyDeploy/sboot && sudo chmod +x startup.sh && sudo ./startup.sh >/dev/null 2>&1"
