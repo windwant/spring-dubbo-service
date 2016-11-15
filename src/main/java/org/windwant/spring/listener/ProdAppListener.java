41365c3557a0fc227664e93d69a86b3a96b70537
聂俊雷 <junlei.nie@hotmail.com>
2016-11-15 18:02:30 +0800

    //

---
+++ src/main/java/org/windwant/spring/listener/ProdAppListener.java
@@ -0,0 +1,15 @@
+package org.windwant.spring.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * Created by windwant on 2016/11/9.
+ */
+public class ProdAppListener implements ApplicationListener {
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        System.out.println("Prod ApplicationEvent: " + applicationEvent.getClass().getName());
+    }
+}
---
+++ src/main/java/org/windwant/spring/listener/TestAppListener.java
@@ -0,0 +1,15 @@
+package org.windwant.spring.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * Created by windwant on 2016/11/9.
+ */
+public class TestAppListener implements ApplicationListener {
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        System.out.println("Test ApplicationEvent: " + applicationEvent.getClass().getName());
+    }
+}
