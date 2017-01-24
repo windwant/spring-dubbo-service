55000b8b039fe112c693f40f8a0a93d165992ee7
聂俊雷 <junlei.nie@hotmail.com>
2017-01-24 11:42:40 +0800

    routing

---
+++ src/main/java/org/windwant/spring/datasource/RoutingDataSource.java
@@ -0,0 +1,20 @@
+package org.windwant.spring.datasource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * Created by windwant on 2016/12/30.
+ */
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    private static final ThreadLocal<String> dataSourceKey = new ThreadLocal<>();
+
+    public static void setThreadLocalDataSourceKey(String dataSourceKey){
+        RoutingDataSource.dataSourceKey.set(dataSourceKey);
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return RoutingDataSource.dataSourceKey.get();
+    }
+}
