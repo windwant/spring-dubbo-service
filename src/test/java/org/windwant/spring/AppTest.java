4c7ef90b6d74f6bc35aaebd999a62da2ecae9497
聂俊雷 <junlei.nie@hotmail.com>
2017-05-15 18:08:05 +0800

    spring boot test

--- src/test/java/org/windwant/spring/AppTest.java
+++ src/test/java/org/windwant/spring/AppTest.java
@@ -1,38 +1,29 @@
 package org.windwant.spring;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Unit test for simple BootSpring.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @org.junit.Test
     public void testApp()
     {
-        assertTrue( true );
+        String body = this.restTemplate.getForObject("/hello", String.class);
+        System.out.println(body);
+        assertThat(body).isEqualTo("{\"msg\":\"OK\",\"code\":0}");
     }
 }
