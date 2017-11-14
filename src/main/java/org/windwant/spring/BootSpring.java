f314969b7c0d78dae3560dbf5533fb323bc5b8fb
junlei.nie <junlei.nie@aachuxing.com>
2017-11-14 14:10:38 +0800

    添加：
    Metrics —— JVM上的实时监控类库；
    logback

--- src/main/java/org/windwant/spring/BootSpring.java
+++ src/main/java/org/windwant/spring/BootSpring.java
@@ -1,17 +1,23 @@
 package org.windwant.spring;
 
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Slf4jReporter;
 import org.joda.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.windwant.spring.config.MybatisConfig;
 import org.windwant.spring.service.BootService;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Hello world!
@@ -24,11 +30,18 @@ import org.windwant.spring.service.BootService;
 @ServletComponentScan
 @PropertySource({"classpath:config.properties"})
 //@EnableScheduling //定时任务
-@Import({MybatisConfig.class})
 public class BootSpring
 {
+
+    @Autowired
+    private Meter requestMeter;
+    @Autowired
+    private Counter requestCount;
+
     @RequestMapping("/{name}")
     String home(@PathVariable String name){
+        requestMeter.mark();
+        requestCount.inc();
         return bootService.hello(name);
     }
 
@@ -43,7 +56,14 @@ public class BootSpring
         System.out.println("Hello agin World!" + localTime.toString());
         SpringApplication sa = new SpringApplication(BootSpring.class);
 //        sa.addListeners(new DevAppListener());//注册Listener
-        sa.run(args);
+        ApplicationContext ctx = sa.run(args);
 
+        //启用console metric 输出
+        ConsoleReporter reporter = ctx.getBean(ConsoleReporter.class);
+        reporter.start(1, TimeUnit.SECONDS);
+
+        //slf4jReporter metric 输出
+        Slf4jReporter reporterSJ = ctx.getBean(Slf4jReporter.class);
+        reporterSJ.start(1, TimeUnit.SECONDS);
     }
 }
