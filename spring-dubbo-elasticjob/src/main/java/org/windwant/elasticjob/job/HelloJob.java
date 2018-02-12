a56bd03cbc66cbe7a4defcb72b8b263c1fd731d1
junlei.nie <junlei.nie@aachuxing.com>
2018-02-12 16:23:13 +0800

    job

--- spring-dubbo-elasticjob/src/main/java/org/windwant/elasticjob/job/HelloJob.java
+++ spring-dubbo-elasticjob/src/main/java/org/windwant/elasticjob/job/HelloJob.java
@@ -1,21 +1,33 @@
 package org.windwant.elasticjob.job;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
-import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.windwant.common.util.HttpUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Administrator on 2018/2/12.
  */
-public class HelloJob implements SimpleJob{
+public class HelloJob implements DataflowJob{
     private static final Logger logger = LoggerFactory.getLogger(HelloJob.class);
     private static final String dsUrlTwo = "http://localhost:8081/hello/";
+
     @Override
-    public void execute(ShardingContext shardingContext) {
+    public List fetchData(ShardingContext shardingContext) {
+        return new ArrayList(){{
+            add(ThreadLocalRandom.current().nextInt(5));
+        }};
+    }
+
+    @Override
+    public void processData(ShardingContext shardingContext, List data) {
         logger.info("job {}, param: {}", shardingContext.getJobName(), shardingContext.getShardingParameter());
-        String url = dsUrlTwo + shardingContext.getShardingParameter();
+        String url = dsUrlTwo + shardingContext.getShardingParameter() + data.get(0);
         logger.info("test hello request url {}", url);
         HttpUtils.urlParamGet(url, 2);
     }
