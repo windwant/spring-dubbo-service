83b2236b2ba76705b97b2e5c09712d3d5c3aa28b
windwant <nie.junlei@hotmail.com>
2018-03-23 14:14:30 +0800

    mongodb 集成

---
+++ spring-boot-server/src/main/java/org/windwant/spring/model/Person.java
@@ -0,0 +1,37 @@
+package org.windwant.spring.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Person {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
